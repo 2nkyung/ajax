b5d242051a62277bd6c92462ff5a404400730fe1
2nkyung <92daisychoi@gmail.com>
2019-03-29 16:42:07 +0900

    tttt

--- src/servlet/UserServlet.java
+++ src/servlet/UserServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,32 +19,34 @@ public class UserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	private UserService us = new UserServiceImpl();
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
-		String uiId = request.getParameter("ui_id");
-		String uiPwd = request.getParameter("ui_pwd");
-		String uiName = request.getParameter("ui_name");
-		String uiEmail = request.getParameter("ui_email");
 		String cmd = request.getParameter("cmd");
-		Map<String,String> user = new HashMap<>();
-		user.put("uiId", uiId);
-		user.put("uiPwd", uiPwd);
-		user.put("uiName", uiName);
-		user.put("uiEmail", uiEmail);
-		if("insert".equals(cmd)) {
-			int cnt = 0;
-			if(user.get("uiId") != null) {
-				cnt = us.insertUser(user);
+		if ("insert".equals(cmd)) {
+			String uiId = request.getParameter("ui_id");
+			String uiPwd = request.getParameter("ui_pwd");
+			String uiName = request.getParameter("ui_name");
+			String uiEmail = request.getParameter("ui_email");
+			Map<String, String> user = new HashMap<>();
+			user.put("uiId", uiId);
+			user.put("uiPwd", uiPwd);
+			user.put("uiName", uiName);
+			user.put("uiEmail", uiEmail);
+			if (us.insertUser(user) == 1) {
+				request.setAttribute("msg", "회원가입에 성공하였습니다.");
+				request.setAttribute("url", "/");
 			}
+			RequestDispatcher rd = request.getRequestDispatcher("/views/msg/result");
+			rd.forward(request, response);
 		}
-		System.out.println(user);
 	}
 
 }
